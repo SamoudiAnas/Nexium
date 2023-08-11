efdb199d12f45e54a0f65133bdac66972a1095c9
Anas Samoudi <unlimiteddez@gmail.com>
2023-08-11 23:27:35 +0100

    added ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { isDashboardLayout } from "@/utils/getLayout";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -16,7 +17,21 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className="bg-white">
         <Head>
           <title>Restaurant Project | Samoudi Anas</title>
+
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-REWV2S4TZ3"
+          ></Script>
+          <Script>
+            {` 
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-REWV2S4TZ3');
+            `}
+          </Script>
         </Head>
+
         {isDashboardLayout(pathname) ? (
           <DashboardLayout>
             <Component {...pageProps} />
